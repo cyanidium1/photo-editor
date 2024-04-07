ccf5e21b70fdf1114b3dda59d5145bccbf14530a
Fedir <99473719+cyanidium1@users.noreply.github.com>
2024-04-07 22:57:56 +0200

    v1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 // import AOS from "aos";
-import { Route, Routes } from "react-router-dom";
 
-import SharedLayout from "./components/SharedLayout";
 import Home from "./pages/Home";
-import About from "./pages/About";
+// import About from "./pages/About";
 
 // import "./App.css";
 // import "aos/dist/aos.css";
@@ -15,16 +13,7 @@ const App = () => {
   // }, []);
 
   return (
-    <Routes>
-      <Route path="/" element={<SharedLayout />}>
-        <Route index element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route
-          path="*"
-          element={<p className="text-red-500">Page not found</p>}
-        />
-      </Route>
-    </Routes>
+    <Home />
   );
 };
 
